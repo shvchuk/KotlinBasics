31f7d85782529b43c140cbfdef0ae44d9119a828
Andrzej Szewczuk <andrzej.szewczuk@gmail.com>
2017-05-26 00:09:54 +0200

    Initial commit

---
+++ src/Kotlin.kt
@@ -0,0 +1,22 @@
+package demo
+
+fun main(args: Array<String>){
+    println("Hello, world")
+
+    val name = "Andrew"
+
+    var myAge = 33
+
+    var bigInt: Int = Int.MAX_VALUE
+    var smallInt: Int = Int.MIN_VALUE
+
+    println("Biggest Int: " + bigInt)
+    println("Smallest Int: $smallInt")
+
+    var bigLong: Long = Long.MAX_VALUE
+    var smallLong: Long = Long.MIN_VALUE
+
+    println("Biggest Long: " + bigLong)
+    println("Smallest Long: $smallLong")
+
+}
